9d9cde3dc028d1cea68ebdc425ac02b32fed2132
Vishhal <101860494+vishhalnarkar@users.noreply.github.com>
2025-07-12 22:14:29 +0530

    Speech To Text Added (Vosk)

--- main.py
+++ main.py
@@ -32,6 +32,19 @@ if not GEMINI_API:
 client = genai.Client(api_key=GEMINI_API)
 print("Environment Variables Loaded Successfully.\n")
 
+# Constants for Speech-to-Text
+SAMPLE_RATE = 16000
+BLOCK_SIZE = 8000
+MODEL_PATH = "models/vosk-model-en-in-0.5"
+
+# Loading the Vosk model
+model = Model(MODEL_PATH)
+recognizer = KaldiRecognizer(model, SAMPLE_RATE)
+recognizer.SetWords(True)
+
+# Queue for audio input
+audio_queue = queue.Queue()
+
 # Function to request Gemini API and handle the response
 def RequestGeminiAPI(prompt):
     if not prompt:
@@ -62,6 +75,28 @@ def audio_callback(indata, frames, time, status):
         print(f"Audio input status: {status}")
     audio_queue.put(bytes(indata))
 
+def listen(timeout: int = 5) -> str:
+    # result_text = ""
+    result_text = "Error: No speech detected."
+    with sd.RawInputStream(samplerate=SAMPLE_RATE,
+                           blocksize=BLOCK_SIZE,
+                           dtype='int16',
+                           channels=1,
+                           callback=audio_callback):
+        print("Listening...")
 
+        total_chunks = int((timeout * SAMPLE_RATE) / BLOCK_SIZE)
+        for _ in range(total_chunks):
+            data = audio_queue.get()
+            if recognizer.AcceptWaveform(data):
+                result = json.loads(recognizer.Result())
+                result_text = result.get("text", "")
+                break  
 
-say(RequestGeminiAPI("I infact am, channeling Walter White, I am the one who knocks!"))
+        if not result_text:
+            partial = json.loads(recognizer.PartialResult())
+            result_text = partial.get("partial", "")
+    print(f"Recognized Text: {result_text}\n")
+    return result_text.strip()
+
+say(listen(timeout=5))
