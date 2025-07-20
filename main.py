2bdb374d1f422d6a4fd182d478daeab564ec27ce
Vishhal <101860494+vishhalnarkar@users.noreply.github.com>
2025-07-21 00:29:53 +0530

    Search Logic Partial

--- main.py
+++ main.py
@@ -13,5 +13,22 @@ from features import google_search
 # Testing the Code after updates
 if __name__ == "__main__":
     # say(RequestGeminiAPI(listen()))
-    open_website("weather today")
+    # open_website("weather today")
     
+    sentence = "Open YouTube and search".lower()
+    if "open" in sentence:
+        open_website(sentence)
+    elif "search" in sentence:
+        google_search(sentence.replace("search", "").strip())
+    else:
+        print("No valid command found in the command.")
+        say("No valid command found in the command.")
+
+'''
+open websites 
+search google
+open desktop files 
+delete files
+open applications
+
+'''
